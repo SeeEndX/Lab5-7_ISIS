19e4fdffa77618809839ac992312e97147bcd70f
Арсений Сендецкий <ars_121229@mail.ru>
2022-11-03 13:15:31 +0800

    сделано заполнение таблицы excel

--- Lab5_ISIS/Form1.Designer.cs
+++ Lab5_ISIS/Form1.Designer.cs
@@ -33,13 +33,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 59);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(29, 32);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 41);
+            this.button1.Size = new System.Drawing.Size(384, 61);
             this.button1.TabIndex = 0;
             this.button1.Text = "Создать и заполнить таблицу";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,43 +53,101 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(91, 228);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(200, 218);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 37);
+            this.button2.Size = new System.Drawing.Size(213, 43);
             this.button2.TabIndex = 1;
             this.button2.Text = "Заполнить ячейки";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 40);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.Size = new System.Drawing.Size(388, 20);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Word";
+            this.label1.Text = "Создание документа Word формата .doc";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(91, 200);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(29, 215);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(267, 22);
+            this.textBox1.Size = new System.Drawing.Size(57, 41);
             this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(91, 178);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 160);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.Size = new System.Drawing.Size(393, 20);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Excel";
+            this.label2.Text = "Создание документа Excel формата .xlsx";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(128, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 41);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(92, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 36);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "x";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(26, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Размерность таблицы";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Строки";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 263);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Столбцы";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(416, 312);
+            this.ClientSize = new System.Drawing.Size(443, 311);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -103,6 +167,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Lab5_ISIS/Form1.cs
+++ Lab5_ISIS/Form1.cs
@@ -15,6 +15,7 @@ using Microsoft.Office.Interop.Word;
 using MSOfficeManager.API;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System.Drawing.Text;
+using System.Windows.Forms.VisualStyles;
 
 namespace Lab5_ISIS
 {
@@ -25,7 +26,7 @@ namespace Lab5_ISIS
             InitializeComponent();
         }
 
-        
+        //word
         private void button1_Click(object sender, EventArgs e)
         {
             Random random = new Random();
@@ -53,7 +54,7 @@ namespace Lab5_ISIS
             table.Cell(2, 1).Range.Text = "Ноутбук Lenovo Legion 5";
             table.Cell(2, 2).Range.Text = price1.ToString();
             table.Cell(2, 3).Range.Text = amm1.ToString();
-            table.Cell(2, 4).Range.Text = (price1*amm1).ToString();
+            table.Cell(2, 4).Range.Text = (price1 * amm1).ToString();
 
             table.Cell(3, 1).Range.Text = "Мышка Logitech G102";
             table.Cell(3, 2).Range.Text = price2.ToString();
@@ -61,5 +62,59 @@ namespace Lab5_ISIS
             table.Cell(3, 4).Range.Text = (price2 * amm2).ToString();
             app.Visible = true;
         }
+
+        //excel
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            Excel.Application appE = new Excel.Application();
+            Excel.Workbook workbook = appE.Workbooks.Add();
+            appE.SheetsInNewWorkbook = 1;
+            Excel.Worksheet worksheet = (Excel.Worksheet)appE.Worksheets.get_Item(1);
+
+            if (this.textBox2.Text != String.Empty && this.textBox1.Text != String.Empty)
+            {
+                int i1 = int.Parse(this.textBox2.Text);
+                int j1 = int.Parse(this.textBox1.Text);
+                for (int i = 1; i <= i1; i++)
+                {
+                    for (int j = 1; j <= j1; j++)
+                        worksheet.Cells[i, j] = 3;
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= 6; i++)
+                {
+                    for (int j = 1; j <= 6; j++)
+                        worksheet.Cells[i, j] = i + j;
+                }
+            }
+            // textBox2 столбцы
+            // textBox1 строки
+            appE.Visible = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
     }
 }
